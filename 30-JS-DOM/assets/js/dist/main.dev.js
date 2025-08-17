b899bad9034d849e8c96e8b5393427099d7583d9
Skazko_o <skazko@outlook.com>
2025-08-18 00:14:58 +0300

    Виконав ДЗ-30 DOM

---
+++ 30-JS-DOM/assets/js/dist/main.dev.js
@@ -0,0 +1,152 @@
+"use strict";
+
+//Створити сторінку, що показує нумерований список пісень
+var playList = [{
+  author: "LED ZEPPELIN",
+  song: "STAIRWAY TO HEAVEN"
+}, {
+  author: "QUEEN",
+  song: "BOHEMIAN RHAPSODY"
+}, {
+  author: "LYNYRD SKYNYRD",
+  song: "FREE BIRD"
+}, {
+  author: "DEEP PURPLE",
+  song: "SMOKE ON THE WATER"
+}, {
+  author: "JIMI HENDRIX",
+  song: "ALL ALONG THE WATCHTOWER"
+}, {
+  author: "AC/DC",
+  song: "BACK IN BLACK"
+}, {
+  author: "QUEEN",
+  song: "WE WILL ROCK YOU"
+}, {
+  author: "METALLICA",
+  song: "ENTER SANDMAN"
+}];
+var main = document.createElement("main");
+main.id = "main-content";
+var div = document.createElement("div");
+div.className = "container";
+addElement(main, div);
+var h1 = document.createElement("h1");
+h1.style.margin = "10px 0";
+h1.append("Song's List");
+addElement(div, h1);
+var ol = document.createElement("ol");
+ol.id = "list";
+addElement(div, ol);
+addListElement();
+
+function addListElement() {
+  playList.forEach(function (song) {
+    var li = document.createElement("li");
+    li.style.paddingBottom = "5px";
+    li.textContent = "".concat(song.author, " - ").concat(song.song);
+    ol.appendChild(li);
+  });
+}
+
+function addElement(parent, tag) {
+  parent.appendChild(tag);
+  document.body.appendChild(parent);
+}
+/*
+const itemInput = document.querySelector("#itemInput");
+const addButton = document.querySelector("#addButton");
+const list = document.querySelector("#list");
+
+itemInput.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        addItem();
+    }
+})
+
+addButton.addEventListener("click", addItem)
+
+function addItem() {
+    const listItem = document.createElement("li");
+    const textNode = document.createTextNode(itemInput.value);
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.style.margin = "2px 5px";
+    removeButton.addEventListener("click", removeItem);
+
+    listItem.append(textNode, removeButton);
+
+    list.prepend(listItem);
+
+    itemInput.value = "";
+    itemInput.focus();
+}
+
+function removeItem(e) {
+    const removeButton = e.target;
+    const listItem = removeButton.parentElement;
+    listItem.remove();
+}
+
+const div = document.querySelector("#app")
+
+const p = document.createElement("p");
+
+p.textContent = "Welcome to our coffee shop";
+
+div.append(p);
+
+const h1 = document.querySelector("h1")
+h1.textContent = "Skazko";
+
+const body = document.querySelector("body");
+body.style.backgroundColor = "black";
+body.style.color = "green"
+
+
+ //як пройтись по дереву нод використовуючи рекурсію
+walkNode(document);
+
+function walkNode(node) {
+    if (node.nodeType === Node.ELEMENT_NODE) { // відоюразити тільки елементи нод
+    console.log(node.nodeName);
+    }
+    node.childNodes.forEach(child => walkNode(child));
+}
+
+ //як пройтись по дереву нод за допомогою обєкта TreeWalker
+const treeWalker = document.createTreeWalker(
+    document,NodeFilter.SHOW_ELEMENT);
+
+    let node = treeWalker.nextNode();
+
+    while(node){
+        if(node instanceof HTMLParagraphElement) {
+            node.style.backgroundColor = "#CCC";
+        }
+        console.log(node.nodeName);
+        node = treeWalker.nextNode();
+    }
+
+// достукатись до елемента
+const elements = document.getElementsByTagName("p");
+let count = 1;
+for ( const el of elements) {
+el.style.backgroundColor = "#CCC";
+el.textContent = `${count++}. ${el.textContent}`;
+}
+
+
+const elements = document.getElementsByClassName("list");
+let count = 1;
+for ( const el of elements) {
+el.style.backgroundColor = "#CCC";
+el.textContent = `${count++}. ${el.textContent}`;
+}
+
+
+const element = document.querySelectorAll("#app .list");
+for (const el of element) {
+el.style.backgroundColor = "#CCC";
+}
+*/
