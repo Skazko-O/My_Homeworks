66270c9029f6162b43bb5a2bb48b2558a8db9f16
Skazko_o <skazko@outlook.com>
2025-08-21 12:01:21 +0300

    Refactor editable text zone functionality and styles

--- 31-JS-Event/assets/js/dist/main.dev.js
+++ 31-JS-Event/assets/js/dist/main.dev.js
@@ -2,22 +2,21 @@
 
 var h1 = document.querySelector('h1');
 var txtzone = document.createElement('div');
-var textarea = document.createElement('textarea');
-txtzone.innerText = "\n\u0426\u0435 \u0442\u0435\u0441\u0442\u043E\u0432\u0438\u0439 \u0442\u0435\u043A\u0441\u0442 \u0434\u043B\u044F \u0440\u0435\u0434\u0430\u0433\u0443\u0432\u0430\u043D\u043D\u044F!";
+txtzone.textContent = 'Це тестовий текст для редагування!';
 txtzone.className = 'txtzone';
+txtzone.setAttribute('contenteditable', 'false');
 h1.insertAdjacentElement('afterend', txtzone);
 document.addEventListener('keydown', function (e) {
   if (e.ctrlKey && e.code === 'KeyE') {
     e.preventDefault();
-    txtzone.replaceWith(textarea);
-    textarea.value = txtzone.textContent;
-    textarea.id = 'editor';
-    textarea.className = 'editor';
+    txtzone.setAttribute('contenteditable', 'true');
+    txtzone.classList.add('edit');
+    txtzone.focus();
   }
 
   if (e.ctrlKey && e.code === 'KeyS') {
     e.preventDefault();
-    textarea.replaceWith(txtzone);
-    txtzone.textContent = textarea.value;
+    txtzone.setAttribute('contenteditable', 'false');
+    txtzone.classList.remove('edit');
   }
 });
--- 31-JS-Event/assets/js/main.js
+++ 31-JS-Event/assets/js/main.js
@@ -1,22 +1,23 @@
 const h1 = document.querySelector('h1');
 const txtzone = document.createElement('div');
-const textarea = document.createElement('textarea');
-txtzone.innerText = `
-Це тестовий текст для редагування!`;
-txtzone.className = 'txtzone'
+
+txtzone.textContent = 'Це тестовий текст для редагування!';
+txtzone.className = 'txtzone';
+txtzone.setAttribute('contenteditable', 'false');
+
 h1.insertAdjacentElement('afterend', txtzone);
 
 document.addEventListener('keydown', (e) => {
     if (e.ctrlKey && e.code === 'KeyE') {
-        e.preventDefault();        
-        txtzone.replaceWith(textarea);
-        textarea.value = txtzone.textContent;
-        textarea.id = 'editor';
-        textarea.className = 'editor';
+        e.preventDefault();
+        txtzone.setAttribute('contenteditable', 'true')
+        txtzone.classList.add('edit')
+        txtzone.focus();
     }
+    
     if (e.ctrlKey && e.code === 'KeyS') {
         e.preventDefault();
-        textarea.replaceWith(txtzone);
-        txtzone.textContent = textarea.value;
+        txtzone.setAttribute('contenteditable', 'false');
+        txtzone.classList.remove('edit');
     }
 })
